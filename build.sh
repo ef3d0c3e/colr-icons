fe52d3e15f899b2f60c05cec7be2c3dbd40cef69
ef3d0c3e <ef3d0c3e@pundalik.org>
2026-06-28 16:06:06 +0200

    Add gitignore

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mkdir -p build
 export PYTHONPATH="$PWD"
 nanoemoji \
   --color_format glyf_colr_1 \
-  --output_file icons.ttf \
+  --output_file 'COLR Icons.ttf' \
   --keep_glyph_names \
   --family 'COLR Terminal Icons' \
   --glyphmap_generator glyphmap \
